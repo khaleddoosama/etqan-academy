54b3e4bb18e68a91aa2e509197f8bb493a10be0b
khaleddoosama <khaleddoosama@gmail.com>
2024-03-23 03:11:49 +0200

    add procfile file for heroku

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\admin\AdminController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CourseController;
 use App\Http\Controllers\Admin\LectureController;

222fbf8fde639495cc281f419e81b663ae09c8f9
khaleddoosama <khaleddoosama@gmail.com>
2024-07-16 14:18:05 +0300

    fix errors in server

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -121,13 +121,21 @@ class ConvertVideoForStreaming implements ShouldQueue
             Log::info('Processing video: ' . $this->lecture->video);
 
             $videoPath = storage_path('app/public/' . $this->lecture->video);
+            $directoryPath = dirname($videoPath);
+
+            // Check if directory exists, if not create it
+            if (!file_exists($directoryPath)) {
+                Log::info('Creating directory: ' . $directoryPath);
+                mkdir($directoryPath, 0777, true);
+            }
+            
             // Log the directory contents and permissions
             Log::info('Directory contents: ' . implode(', ', scandir(dirname($videoPath))));
             Log::info('File permissions: ' . substr(sprintf('%o', fileperms($videoPath)), -4));
 
             $video1 = $ffprobe->streams($videoPath)->videos()->first();
 
-            Log::info('Video Path: ' . storage_path('')); 
+            Log::info('Video Path: ' . storage_path(''));
 
             $width = $video1->get('width');
             $height = $video1->get('height');

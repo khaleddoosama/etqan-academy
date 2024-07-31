d038dcde0fb8a946aa0d5e4467514e8c6a6bf7fa
khaleddoosama <khaleddoosama@gmail.com>
2024-08-01 02:00:07 +0300

    add logo

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -10,7 +10,8 @@
     <!-- ==================== styles ====================== -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-
+    {{-- icon --}}
+    <link rel="icon" href="{{ asset('asset/logo.png') }}">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
 

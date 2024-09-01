ac0aa12a7c0c439a8b4b3733b83fe78376bc944c
khaleddoosama <khaleddoosama@gmail.com>
2024-09-01 22:25:01 +0300

    edit logo

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -11,7 +11,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- icon --}}
-    <link rel="icon" href="{{ asset('asset/logo.png') }}">
+    <link rel="icon" href="{{ asset('asset/logo.jpg') }}">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css">
 
@@ -83,7 +83,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset('asset/logo.png') }}" alt="EtqanLogo"
+            <img class="animation__shake" src="{{ asset('asset/logo.jpg') }}" alt="EtqanLogo"
                 height="60" width="60">
         </div>
 

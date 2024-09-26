d7ecedd58a621d5b4bcdc2b63ff3ec5cefa2f27b
khaleddoosama <khaleddoosama@gmail.com>
2024-09-26 19:36:38 +0300

    send email when student approve direct

--- app/Notifications/StudentApprovedNotification.php
+++ app/Notifications/StudentApprovedNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class StudentApprovedNotification extends Notification implements ShouldQueue
+class StudentApprovedNotification extends Notification
 {
     use Queueable, NotificationToArray;
 

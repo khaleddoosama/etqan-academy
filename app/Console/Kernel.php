2a54e7e53b4916ce2c4ecd2ae87c896f8fdca750
khaleddoosama <khaleddoosama@gmail.com>
2024-12-17 23:27:03 +0200

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:check-installment-access')->daily();
-        $schedule->command('report:weekly-payments')->weeklyOn(5, '00:01');
+        $schedule->command('report:weekly-payments')->weeklyOn(3, '00:01'); // Run on Wednesday at 00:01
     }
 
     /**

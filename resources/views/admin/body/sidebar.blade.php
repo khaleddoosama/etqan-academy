0abcd6a4b186bc22dc1f2b7d2b8414b16254ad8a
khaleddoosama <khaleddoosama@gmail.com>
2025-09-22 15:31:01 +0300

    fix(logo): update logo file extension from .jpg to .png across multiple views

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="{{ route('admin.home') }}" class="brand-link">
-          <img src="{{ asset('asset/logo.jpg') }}" alt="Aura Logo" class="brand-image img-circle elevation-3"
+          <img src="{{ asset('asset/logo.png') }}" alt="Aura Logo" class="brand-image img-circle elevation-3"
               style="opacity: .8">
           <span class="brand-text font-weight-light">{{ __('main.dashboard') }}</span>
       </a>
